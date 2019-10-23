b71cfa709d4a59dd865d6d2ba470f412e0a173f3
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2019-10-23 12:33:40 -0300

    iniciando projeto

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const nodeDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production' , 
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './build',
+        port: 9099,
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: __dirname + '/build',
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
+        new CopyWebpackPlugin ([
+            {context: 'src/',  from: '**/*.html'},
+            {context: 'src/',  from: 'imgs/**/*'},
+        ])
+    ],
+
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                 'sass-loader',
+            ]
+        }, {
+            test:  /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+
+        },{
+            test:  /.(ttf|otf|svg|woff(2)?)$/,
+            use: ['file-loader']
+        }]
+    }
+    
+
+ }
